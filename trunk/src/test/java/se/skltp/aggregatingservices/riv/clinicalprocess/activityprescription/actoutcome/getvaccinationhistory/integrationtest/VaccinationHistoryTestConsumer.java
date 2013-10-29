8101b468973237e3a59ff94aad44f3d92badeae8
hans.thunberg@callistaenterprise.se <hans.thunberg@callistaenterprise.se@95a8c5e1-5168-66ac-ef81-70fca3c1b79b>
2013-10-29 13:22:04 +0000

    [maven-scm] copy for tag GetAggregatedVaccinationHistory-1.0.0-RC4

---
+++ trunk/src/test/java/se/skltp/aggregatingservices/riv/clinicalprocess/activityprescription/actoutcome/getvaccinationhistory/integrationtest/VaccinationHistoryTestConsumer.java
@@ -0,0 +1,54 @@
+package se.skltp.aggregatingservices.riv.clinicalprocess.activityprescription.actoutcome.getvaccinationhistory.integrationtest;
+
+import static se.skltp.agp.test.producer.TestProducerDb.TEST_RR_ID_ONE_HIT;
+
+import javax.xml.ws.Holder;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import se.riv.clinicalprocess.activityprescription.actoutcome.getvaccinationhistory.v1.rivtabp21.GetVaccinationHistoryResponderInterface;
+import se.riv.clinicalprocess.activityprescription.actoutcome.getvaccinationhistoryresponder.v1.GetVaccinationHistoryResponseType;
+import se.riv.clinicalprocess.activityprescription.actoutcome.getvaccinationhistoryresponder.v1.GetVaccinationHistoryType;
+import se.riv.clinicalprocess.activityprescription.actoutcome.v1.PersonIdType;
+import se.skltp.aggregatingservices.VaccinationHistoryMuleServer;
+import se.skltp.agp.riv.interoperability.headers.v1.ProcessingStatusType;
+import se.skltp.agp.test.consumer.AbstractTestConsumer;
+import se.skltp.agp.test.consumer.SoapHeaderCxfInterceptor;
+
+public class VaccinationHistoryTestConsumer extends AbstractTestConsumer<GetVaccinationHistoryResponderInterface>{
+
+    private static final Logger log = LoggerFactory.getLogger(VaccinationHistoryTestConsumer.class);
+
+    public static void main(String[] args) {
+        log.info("URL: " + VaccinationHistoryMuleServer.getAddress("SERVICE_INBOUND_URL"));
+        String serviceAddress = VaccinationHistoryMuleServer.getAddress("SERVICE_INBOUND_URL");
+        String personnummer = TEST_RR_ID_ONE_HIT;
+
+        VaccinationHistoryTestConsumer consumer = new VaccinationHistoryTestConsumer(serviceAddress, SAMPLE_ORIGINAL_CONSUMER_HSAID);
+        Holder<GetVaccinationHistoryResponseType> responseHolder = new Holder<GetVaccinationHistoryResponseType>();
+        Holder<ProcessingStatusType> processingStatusHolder = new Holder<ProcessingStatusType>();
+        long now = System.currentTimeMillis();
+        consumer.callService("logical-adress", personnummer, processingStatusHolder, responseHolder);
+        log.info("Returned #vaccination record= " + responseHolder.value.getVaccinationMedicalRecord().size() + " in " + (System.currentTimeMillis() - now) + " ms.");	
+    }
+
+    public VaccinationHistoryTestConsumer(String serviceAddress, String originalConsumerHsaId) {
+        // Setup a web service proxy for communication using HTTPS with Mutual Authentication
+        super(GetVaccinationHistoryResponderInterface.class, serviceAddress, originalConsumerHsaId); 
+    }
+
+    public void callService(String logicalAddress, String id, Holder<ProcessingStatusType> processingStatusHolder, Holder<GetVaccinationHistoryResponseType> responseHolder) {
+        log.debug("Calling GetAggregatedVaccinationHistory-soap-service with id = {}", id);
+
+        GetVaccinationHistoryType request = new GetVaccinationHistoryType();
+        PersonIdType patientId = new PersonIdType();
+        patientId.setId(id);
+        request.setPatientId(patientId);
+
+        GetVaccinationHistoryResponseType response = _service.getVaccinationHistory(logicalAddress, request);
+        responseHolder.value = response;
+
+        processingStatusHolder.value = SoapHeaderCxfInterceptor.getLastFoundProcessingStatus();
+    }
+}
---
+++ trunk/src/test/java/se/skltp/aggregatingservices/riv/clinicalprocess/activityprescription/actoutcome/getvaccinationhistory/integrationtest/VaccinationHistoryTestProducerDb.java
@@ -0,0 +1,79 @@
+package se.skltp.aggregatingservices.riv.clinicalprocess.activityprescription.actoutcome.getvaccinationhistory.integrationtest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import se.riv.clinicalprocess.activityprescription.actoutcome.getvaccinationhistoryresponder.v1.GetVaccinationHistoryResponseType;
+import se.riv.clinicalprocess.activityprescription.actoutcome.v1.HealthcareProfessionalType;
+import se.riv.clinicalprocess.activityprescription.actoutcome.v1.LegalAuthenticatorType;
+import se.riv.clinicalprocess.activityprescription.actoutcome.v1.OrgUnitType;
+import se.riv.clinicalprocess.activityprescription.actoutcome.v1.PatientSummaryHeaderType;
+import se.riv.clinicalprocess.activityprescription.actoutcome.v1.PersonIdType;
+import se.riv.clinicalprocess.activityprescription.actoutcome.v1.RegistrationRecordType;
+import se.riv.clinicalprocess.activityprescription.actoutcome.v1.VaccinationMedicalRecordBodyType;
+import se.riv.clinicalprocess.activityprescription.actoutcome.v1.VaccinationMedicalRecordType;
+import se.skltp.agp.test.producer.TestProducerDb;
+
+public class VaccinationHistoryTestProducerDb extends TestProducerDb {
+
+    private static final Logger log = LoggerFactory.getLogger(VaccinationHistoryTestProducerDb.class);
+
+    @Override
+    public Object createResponse(Object... responseItems) {
+        log.debug("Creates a response with {} items", responseItems);
+        GetVaccinationHistoryResponseType response = new GetVaccinationHistoryResponseType();
+        for (int i = 0; i < responseItems.length; i++) {
+            response.getVaccinationMedicalRecord().add((VaccinationMedicalRecordType)responseItems[i]);
+        }
+        return response;
+    }
+
+    public static final String TEST_REASON_DEFAULT = "default reason";
+    public static final String TEST_REASON_UPDATED = "updated reason";
+
+    @Override
+    public Object createResponseItem(String logicalAddress, String registeredResidentId, String businessObjectId, String time) {
+
+        log.debug("Created one response item for logical-address {}, registeredResidentId {} and businessObjectId {}",
+                new Object[] {logicalAddress, registeredResidentId, businessObjectId});
+
+        VaccinationMedicalRecordType response = new VaccinationMedicalRecordType();
+        PatientSummaryHeaderType header = new PatientSummaryHeaderType();
+        PersonIdType patientId = new PersonIdType();
+        patientId.setId(registeredResidentId);
+        patientId.setType("1.2.752.129.2.1.3.1");
+        header.setPatientId(patientId);
+        header.setApprovedForPatient(true);
+        LegalAuthenticatorType legalAuthenticator = new LegalAuthenticatorType();
+        legalAuthenticator.setSignatureTime("20130707070707");
+        header.setLegalAuthenticator(legalAuthenticator);
+        header.setSourceSystemHSAId(logicalAddress);
+        header.setDocumentId(businessObjectId);
+        header.setDocumentTime(time);
+        
+        // TODO: Set stuff
+
+        VaccinationMedicalRecordBodyType body = new VaccinationMedicalRecordBodyType();
+        
+        RegistrationRecordType regRecord = new RegistrationRecordType();
+        // TODO: Set stuff
+        
+        body.setRegistrationRecord(regRecord);
+        
+        HealthcareProfessionalType author = new HealthcareProfessionalType();
+        author.setHealthcareProfessionalCareUnitHSAId(logicalAddress); // TODO ???
+        OrgUnitType orgUnit = new OrgUnitType();
+        if(TestProducerDb.TEST_LOGICAL_ADDRESS_1.equals(logicalAddress)){
+            orgUnit.setOrgUnitName("Vårdcentralen Kusten, Kärna"); // TODO ???
+        } else if(TestProducerDb.TEST_LOGICAL_ADDRESS_2.equals(logicalAddress)){
+        	orgUnit.setOrgUnitName("Vårdcentralen Molnet");
+        } else {
+        	orgUnit.setOrgUnitName("Vårdcentralen Stacken");
+        }
+        author.setHealthcareProfessionalOrgUnit(orgUnit);
+        header.setAccountableHealthcareProfessional(author);
+        response.setVaccinationMedicalRecordHeader(header);
+        response.setVaccinationMedicalRecordBody(body);
+        return response;
+    }
+}
