ae3bbca7936dc637e7a762d9fdd3344b3b408eba
Martin Flower <martin.flower@callistaenterprise.se>
2015-06-17 09:38:26 +0200

    SERVICE-213 v2.0.0

--- GetAggregatedVaccinationHistory/src/main/java/se/skltp/aggregatingservices/riv/clinicalprocess/activityprescription/actoutcome/getaggregatedvaccinationhistory/ResponseListFactoryImpl.java
+++ GetAggregatedVaccinationHistory/src/main/java/se/skltp/aggregatingservices/riv/clinicalprocess/activityprescription/actoutcome/getaggregatedvaccinationhistory/ResponseListFactoryImpl.java
@@ -6,8 +6,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.soitoolkit.commons.mule.jaxb.JaxbUtil;
 
+import riv.clinicalprocess.activityprescription.actoutcome.enums.v2.ResultCodeEnum;
 import riv.clinicalprocess.activityprescription.actoutcome.getvaccinationhistoryresponder.v2.GetVaccinationHistoryResponseType;
 import riv.clinicalprocess.activityprescription.actoutcome.getvaccinationhistoryresponder.v2.ObjectFactory;
+import riv.clinicalprocess.activityprescription.actoutcome.v2.ResultType;
 import se.skltp.agp.riv.interoperability.headers.v1.ProcessingStatusType;
 import se.skltp.agp.service.api.QueryObject;
 import se.skltp.agp.service.api.ResponseListFactory;
@@ -25,12 +27,11 @@ public class ResponseListFactoryImpl implements ResponseListFactory {
             final GetVaccinationHistoryResponseType response = (GetVaccinationHistoryResponseType) object;
             aggregatedResponse.getVaccinationMedicalRecord().addAll(response.getVaccinationMedicalRecord());
         }
-        if (log.isInfoEnabled()) {
-            String subjectOfCareId = queryObject.getFindContent().getRegisteredResidentIdentification();
-            log.info("Returning {} aggregated alert informations for subject of care id {}", 
-                      aggregatedResponse.getVaccinationMedicalRecord().size(), subjectOfCareId);
-        }
-
+        log.info("Returning {} aggregated alert informations for subject of care id {}", 
+                  aggregatedResponse.getVaccinationMedicalRecord().size(),
+                  queryObject.getFindContent().getRegisteredResidentIdentification());
+        aggregatedResponse.setResult(new ResultType());
+        aggregatedResponse.getResult().setResultCode(ResultCodeEnum.INFO);
         // Since the class GetAlertInformationResponseType doesn't have an @XmlRootElement annotation
         // we need to use the ObjectFactory to add it.
         return jaxbUtil.marshal(OF.createGetVaccinationHistoryResponse(aggregatedResponse));
