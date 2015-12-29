2861273be932721893084325b105da2d193d26df
Martin Flower <martin.flower@callistaenterprise.se>
2015-12-29 15:44:08 +0100

    SERVICE-415 - Upgrade to AgP 1.4.0

--- GetAggregatedVaccinationHistory-teststub/src/test/java/se/skltp/aggregatingservices/riv/clinicalprocess/activityprescription/actoutcome/getaggregatedvaccinationhistory/GetAggregatedVaccinationHistoryTeststubMuleServer.java
+++ GetAggregatedVaccinationHistory-teststub/src/test/java/se/skltp/aggregatingservices/riv/clinicalprocess/activityprescription/actoutcome/getaggregatedvaccinationhistory/GetAggregatedVaccinationHistoryTeststubMuleServer.java
@@ -1,6 +1,6 @@
 package se.skltp.aggregatingservices.riv.clinicalprocess.activityprescription.actoutcome.getaggregatedvaccinationhistory;
 
- 
+
 import org.soitoolkit.commons.mule.test.StandaloneMuleServer;
 
 import org.soitoolkit.commons.mule.util.RecursiveResourceBundle;
@@ -11,38 +11,34 @@ import org.slf4j.LoggerFactory;
 
 public class GetAggregatedVaccinationHistoryTeststubMuleServer {
 
-
-	public static final String MULE_SERVER_ID   = "GetAggregatedVaccinationHistory-teststub";
- 
+	public static final String MULE_SERVER_ID = "GetAggregatedVaccinationHistory-teststub";
 
 	private static final Logger logger = LoggerFactory.getLogger(GetAggregatedVaccinationHistoryTeststubMuleServer.class);
     private static final RecursiveResourceBundle rb = new RecursiveResourceBundle("GetAggregatedVaccinationHistory-config");
 
 	public static void main(String[] args) throws Exception {
 
- 
         // Configure the mule-server
         // Note: do not activate the "soitoolkit-teststubs" profile here since it sets a
         // system property, which is JVM global and may activate all teststubs in a Mule-instance.
         // Teststubs are always loaded by the top-level config.xml file.
         StandaloneMuleServer muleServer = new StandaloneMuleServer(MULE_SERVER_ID, false, true);
- 
+
         // Start the server
-		muleServer.run();
+        muleServer.run();
 	}
 
     /**
      * Address based on usage of the servlet-transport and a config-property for the URI-part
-     * 
+     *
      * @param serviceUrlPropertyName
      * @return
      */
     public static String getAddress(String serviceUrlPropertyName) {
 
         String url = rb.getString(serviceUrlPropertyName);
+        logger.info("URL: {}", url);
+        return url;
 
-	    logger.info("URL: {}", url);
-    	return url;
- 
-    }	
+    }
 }
