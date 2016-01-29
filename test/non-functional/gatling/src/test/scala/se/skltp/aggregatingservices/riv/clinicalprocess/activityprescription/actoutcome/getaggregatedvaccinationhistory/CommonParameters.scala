e4e36f2f04a16a11140f6734d452753908a9df53
hanswiklund <hans.wiklund@kentor.se>
2016-01-29 18:04:14 +0100

    gatling: fixed bad wsdl-namespace in test that caused failure due to a non-matching soapAction being used

--- test/non-functional/gatling/src/test/scala/se/skltp/aggregatingservices/riv/clinicalprocess/activityprescription/actoutcome/getaggregatedvaccinationhistory/CommonParameters.scala
+++ test/non-functional/gatling/src/test/scala/se/skltp/aggregatingservices/riv/clinicalprocess/activityprescription/actoutcome/getaggregatedvaccinationhistory/CommonParameters.scala
@@ -2,9 +2,10 @@ package se.skltp.aggregatingservices.riv.clinicalprocess.activityprescription.ac
 
 trait CommonParameters {
   val serviceName:String     = "VaccinationHistory"
-  val urn:String             = "urn:riv:clinicalprocess:activityprescription:actoutcome:GetVaccinationHistoryResponder:1"
+  val urn:String             = "urn:riv:clinicalprocess:activityprescription:actoutcome:GetVaccinationHistoryResponder:2"
   val responseElement:String = "GetVaccinationHistoryResponse"
   val responseItem:String    = "vaccinationMedicalRecord"
+  val responseItemUrn:String = "urn:riv:clinicalprocess:activityprescription:actoutcome:GetVaccinationHistoryResponder:1"
   var baseUrl:String         = if (System.getProperty("baseUrl") != null && !System.getProperty("baseUrl").isEmpty()) {
                                    System.getProperty("baseUrl")
                                } else {
